2ef8e408856b46eca056378bbb7f0d3038df77f5
chuanchenzz@163.com <chuanchenzz@163.com>
2018-07-23 23:45:13 +0800

    init

---
+++ src/main/java/com/outsource/AppApplication.java
@@ -0,0 +1,17 @@
+package com.outsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author chuanchen
+ */
+@SpringBootApplication
+public class AppApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AppApplication.class,args);
+    }
+}
---
+++ src/test/java/com/outsource/AppApplicationTest.java
@@ -0,0 +1,20 @@
+package com.outsource;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple AppApplication.
+ */
+public class AppApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
